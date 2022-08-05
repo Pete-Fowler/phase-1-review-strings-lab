9bc08b30a29e34f7cb8d2868c99f4b06646d5dd3
Pete Fowler <fowler.pete@gmail.com>
2022-08-04 20:10:08 -0600

    commit tree e3cfebbfe80e55d5830306a0351273bbddfae492

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+const currentUser = 'Pete';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = `${welcomeMessage.toUpperCase()}, + ${currentUser.toUpperCase}!`;
+
+let shortGreeting = `${welcomeMessage.slice(0, 7)}, ${currentUser.slice(0, 1)}!`;
